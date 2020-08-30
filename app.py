778b70c523b69ae8a15a291df62c7455ddf89aa7
SimonWT <y.yudinskikh@innopolis.ru>
2020-08-31 00:04:30 +0300

    make it better

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-@app.route('/{}'.format(TOKEN), methods=['POST'])
+@app.route('/telegram{}'.format(TOKEN), methods=['POST'])
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
@@ -30,7 +30,7 @@ def respond():
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    s = bot.setWebhook('{URL}/telegram{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1078940030:AAHWqHSB0-em4AS_1dLmJmuwJkAubne78wE"
 bot_user_name = "attractions_lifebot"
-URL = "https://places-bot.herokuapp.com/"
+URL = "https://places-bot.herokuapp.com"
